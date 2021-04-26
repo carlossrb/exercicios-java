507d4a60c2a603961293f8a9026dd8ed7236a4ad
Carlos <carlos.ribeiro.brito@hotmail.com>
2021-04-26 04:31:04 -0300

    novo package

---
+++ src/lista01/ConversaoDeUnidadesDeArea.java
@@ -0,0 +1,19 @@
+package lista01;
+
+public class ConversaoDeUnidadesDeArea {
+    public static double metro2ParaPe2(double value){
+        return value * 10.76;
+    }
+
+    public static double pe2ToCm2(double value){
+        return value * 929;
+    }
+
+    public static double milha2ParaAcres(double value){
+        return value * 640;
+    }
+
+    public static double AcreParaPe2(double value){
+        return value * 43560;
+    }
+}
